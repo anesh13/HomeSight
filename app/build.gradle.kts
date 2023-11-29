e1b447b61e39a016a008d53606f4642a2c0e565b
Anesha <anesha.sargunasekarapandian@sjsu.edu>
2023-11-28 18:47:14 -0800

    Firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,4 +38,11 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    //firebase
+    implementation (platform("com.google.firebase:firebase-bom:29.3.1"))
+    implementation ("com.firebaseui:firebase-ui-auth:7.2.0")
+    implementation ("com.google.firebase:firebase-auth")
+    implementation ("com.google.firebase:firebase-firestore")
+    implementation ("com.google.firebase:firebase-messaging:20.1.3")
+    implementation ("com.google.firebase:firebase-analytics")
 }
